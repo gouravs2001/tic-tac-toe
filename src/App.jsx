0eb2ea0149425690b9a6ecfaf7e40dbdf65f3bac
ourav Sharma <gourav27012001@gmail.com>
2022-08-11 23:57:41 +0530

    board styled using scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Board from './Components/Board';
+import './styles/root.scss';
 
 export default () => (
-  <>
+  <div className="app">
     <h1>Tic Tack Toe</h1>
     <Board />
-  </>
+  </div>
 );
