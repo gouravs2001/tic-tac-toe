3a9e65086edda3536a85732d3f769247b0e3705c
ourav Sharma <gourav27012001@gmail.com>
2022-08-14 15:09:35 +0530

    calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Components/Board';
+import { calculateWinner } from './helper.jsx';
 import './styles/root.scss';
 
-export default () => (
-  <div className="app">
-    <h1>Tic Tack Toe</h1>
-    <Board />
-  </div>
-);
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [isXNext, setIsXNext] = useState(false);
+  const winner = calculateWinner(board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+  const handleSquareClick = position => {
+    if (board[position] || winner) return;
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) return isXNext ? 'X' : 'O';
+        return square;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
+  return (
+    <div className="app">
+      <h1>Tic Tack Toe</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
+export default App;
